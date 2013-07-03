5ce58050c82b55e7a1f674c966555350c32e1208
ribbon-xx <mr.ribbon@gmail.com>
2013-07-03 13:56:25 +0700

    Version 3.....

---
+++ src/com/qsoft/bankaccountday4/BankAccountDAO.java
@@ -0,0 +1,16 @@
+package com.qsoft.bankaccountday4;
+
+public class BankAccountDAO {
+
+	public void doCreate(BankAccountDTO baDTO) {
+		return;
+	}
+
+	public BankAccountDTO doRead(String accountNumber) {
+		return null;
+	}
+
+	public BankAccountDTO doUpdate(BankAccountDTO tDTO) {
+		return null;
+	}
+}
